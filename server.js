9e1c0edd6218afe88a8bee142065b8f4548ef7d9
Lopez <carlos.alfonzo@gmail.com>
2017-09-15 19:02:33 -0700

    saved work on server.js file

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
+// Required dependencies 
 var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
 
+// Set up our port to 3000
 var PORT = process.env.PORT || 3000;
 
+// Instantiate our Express app
 var app = express();
 
+// Set up an Express Router
 var router = express.Router();
 
+//Designate our public folder as a static directory
 app.use(express.static(__dirname + "/public"));
 
+// Connect Handlebars to our express app
+app.engine("handlebars", expressHandlebars({
+	defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// Use bodyParser in our app
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+// Have every request go through our router middleware
 app.use(router);
 
+// Listen on the port
 app.listen(PORT, function() {
 	console.log("Listening on port:" + PORT);
 });
